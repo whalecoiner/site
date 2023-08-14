db165d1f168ca6e39b5c958ebda7ef3c3a50f210
Chazza <github@whalecoiner.com>
2023-08-14 17:02:54 +0200

    more 11ty

--- eleventy.config.js
+++ eleventy.config.js
@@ -133,9 +133,9 @@ module.exports = function(eleventyConfig) {
 
 		// These are all optional:
 		dir: {
-			input: "content",          // default: "."
-			includes: "_includes",  // default: "_includes"
-			data: "_data",          // default: "_data"
+			input: "./content",          // default: "."
+			includes: "../_includes",  // default: "_includes"
+			data: "../_data",          // default: "_data"
 			output: "_site"
 		},
 
